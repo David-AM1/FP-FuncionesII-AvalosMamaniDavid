57d050eda361cb488fc2f8713aeaf3128580a7a8
David-AM1 <davalosm@unjbg.edu.pe>
2025-06-11 10:11:46 -0500

    Se agrega funcion para calculo de la hipotenusa, VERSION FINAL

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <cmath>
+#include <windows.h>
 using namespace std;
 
+double CalculoHipotenusa(double a, double b){
+	return sqrt(pow(a,2)+pow(b,2));
+}
+
 int main(){
+	SetConsoleOutputCP(CP_UTF8);
 	double hipotenusa, cateto1, cateto2;
+	cout<<"--- Cálculo de la Hipotenusa ---"<<endl;
 	cout<<"Ingrese el valor del primer cateto: ";
 	cin>>cateto1;
 	cout<<"Ingrese el valor del segundo cateto: ";
 	cin>>cateto2;
-	hipotenusa=sqrt(pow(cateto1,2)+pow(cateto2,2));
-	cout<<"La hipotenusa es: "<<hipotenusa;
+	cout<<"La hipotenusa es: "<<CalculoHipotenusa(cateto1, cateto2);
 }

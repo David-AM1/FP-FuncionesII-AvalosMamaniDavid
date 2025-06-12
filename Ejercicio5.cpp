e217aa59e9f450c61a32a2b52596a11b23eefaf2
David-AM1 <davalosm@unjbg.edu.pe>
2025-06-12 10:52:59 -0500

    Avance2: Se culmina la estructura base de la funcion principal calcularRaices

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -11,39 +11,32 @@ void calcularRaices(double a, double b, double c, double &x1, double &x2, bool &
     	x2=0;
 		cout<<"--- Raíces complejas: ---"<<endl;
 		double imaginario=sqrt(-discriminante)/(2*a);
+		double parteReal=((-b)/(2*a));
 		if(imaginario==1){
-			if(real!=0 or real!=-0){
-			cout<<"Raíz 1 (X1): "<<real<<" + "<<"i"<<endl;
-			cout<<"Raíz 2 (X2): "<<real<<" - "<<"i"<<endl;
-			}else{
-				cout<<"Raíz 1 (X1): + "<<"i"<<endl;
-				cout<<"Raíz 2 (X2): - "<<"i"<<endl;
-			}
+			cout<<"Raíz 1 (X1): "<<parteReal<<" + i"<<endl;
+			cout<<"Raíz 2 (X2): "<<parteReal<<" - i"<<endl;
 		}else{
-			if(real!=0 or real!=-0){
-			cout<<"Raíz 1 (X1): "<<real<<" + "<<imaginario<<"i"<<endl;
-			cout<<"Raíz 2 (X2): "<<real<<" - "<<imaginario<<"i"<<endl;
-			}else{
-				cout<<"Raíz 1 (X1): + "<<imaginario<<"i"<<endl;
-				cout<<"Raíz 2 (X2): - "<<imaginario<<"i"<<endl;
-			}
+			cout<<"Raíz 1 (X1): "<<parteReal<<" + "<<imaginario<<"i"<<endl;
+			cout<<"Raíz 2 (X2): "<<parteReal<<" - "<<imaginario<<"i"<<endl;
 		}
 	}else if (discriminante == 0) {
-		x=-b/(2*a);
-		cout<<"--- Raíz real ---"<<endl;
-        cout<< "La única raíz es (x): " <<x<<endl;
+		sonReales=true;
+		x1=x2=-b/(2*a);
+		cout<<"--- Raíz real (única) ---"<<endl;
+        cout<< "La única raíz es (x): " <<x1<<endl;
 	}else{
+		sonReales=true;
 		cout<<"--- Raíces reales: ---"<<endl;
 		x1=((-b)+sqrt(discriminante))/(2*a);
 		x2=((-b)-sqrt(discriminante))/(2*a);
 		cout<<"Raíz 1 (X1): "<<x1<<endl;
 		cout<<"Raíz 2 (X2): "<<x2;
 	}
-    
-
+}
+  
 int main(){
 	SetConsoleOutputCP(CP_UTF8);
-	double a, b, c, x, x1, x2;
+	double a, b, c, x1, x2;
 	bool sonReales;
 	cout<<"Ingrese el valor del coeficiente 'a': ";
 	cin>>a;
@@ -51,5 +44,5 @@ int main(){
 	cin>>b;
 	cout<<"Ingrese el valor del coeficiente 'c': ";
 	cin>>c;
-	calcularRaices(a,b,c,sonReales);
+	calcularRaices(a,b,c,x1,x2,sonReales);
 }

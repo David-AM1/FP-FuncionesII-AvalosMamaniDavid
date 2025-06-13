c88de3601fc2bf05f495c26ac563e8a0e4a624dc
David-AM1 <davalosm@unjbg.edu.pe>
2025-06-13 00:06:54 -0500

    Avance2: Se arreglan errores en cuanto a la elección de la computadora y mejora la manera de mostrar resultados

--- Ejercicio6.cpp
+++ Ejercicio6.cpp
@@ -3,9 +3,6 @@
 #include <cstdlib>
 using namespace std;
 
-// piedra > tijera
-// tijera > papel
-// papel > piedra
 int juego(int a,int b){
 	bool gana;
 	if (a==1){			//piedra
@@ -13,25 +10,19 @@ int juego(int a,int b){
 			gana=false;
 		}else if(b==3){	//si el rival elige TIJERA
 			gana=true;
-		}//else{			//si el rival elige PIEDRA
-		//	gana=false;
-		//}
+		}
 	}else if(a==2){		//papel
 		if(b==1){		//si el rival elige PIEDRA
 			gana=true;
 		}else if(b==3){	//si el rival elige TIJERA
 			gana=false;
-		}//else{			//si el rival elige PAPEL
-		//	gana=false;
-		//}
+		}
 	}else if(a==3){		//tijera
 		if(b==1){		//si el rival elige PIEDRA
 			gana=false;
 		}else if(b==2){	//si el rival elige PAPEL 
 			gana=true;
-		}//else{			//si el rival elige TIJERA
-		//	gana=false;
-		//}
+		}
 	}
 	return gana;
 }
@@ -42,10 +33,10 @@ int main(){
 	bool ganaRonda;
 	cout<<"--- JUEGO DE PIEDRA, PAPEL Y TIJERAS (victoria al ganador de 3 rondas) ---"<<endl;
 	do{
-	cout<<"Eliga Piedra(1), Papel(2) o Tijeras(3): ";
+	cout<<"Elige Piedra(1), Papel(2) o Tijeras(3): ";
 	cin>>eleccion;
-	cout<<"Turno de la computadora: ... ";
-	eleccionCPU=rand()%4;
+	cout<<"Turno de la computadora ... "<<endl;
+	eleccionCPU=1+rand()%3;
 	switch(eleccionCPU){
 		case 1:
 			cout<<"La computadora eligió piedra"<<endl;
@@ -56,29 +47,26 @@ int main(){
 		case 3:
 			cout<<"La computadora eligió tijera"<<endl;
 			break;
-		default:
-			cout<<"ERROR"<<endl;
-			break;
 	}
 	ganaRonda=juego(eleccion, eleccionCPU);
 	if(ganaRonda){
 		rondasConc++;
 		victorias++;
-		cout<<"Ganaste esta ronda."<<endl;
-		cout<<"Siguiente ronda ..."<<endl;
-	}else if(eleccion == eleccionCPU){
-		cout<<"Empate. Siguiente ronda"<<endl;
+		cout<<"Ganaste esta ronda."<<endl<<endl;
+	}else if(eleccion == eleccionCPU){				//si los dos eligen lo mismo EMPATE
+		cout<<"Empate. Siguiente ronda"<<endl<<endl;
 	}else{
 		rondasConc++;
 		victoriasCPU++;
-		cout<<"Perdiste. La computadora ganó esta ronda..."<<endl;
-		cout<<"Siguiente ronda..."<<endl;
+		cout<<"Perdiste esta ronda."<<endl<<endl;
+	}
+	if(victorias!=3 and victoriasCPU!=3){
+		cout<<"Siguiente ronda ..."<<endl;
 	}
 	}while(victorias!=3 and victoriasCPU!=3);
 	if(victorias==3){
 		cout<<"FELICIDADES, HAS GANADO 3 RONDAS."<<endl;
 	}else if(victoriasCPU==3){
 		cout<<"HAS PERDIDO, LA COMPUTADORA GANÓ 3 RONDAS."<<endl;
-	}
-	
+	}	
 }
